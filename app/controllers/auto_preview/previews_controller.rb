a2ba1e4e006185b212ffae15c22eb698f2482ae5
Joel Hawksley <joelhawksley@github.com>
2026-01-22 12:11:29 -0700

    gitignore etc

--- app/controllers/auto_preview/previews_controller.rb
+++ app/controllers/auto_preview/previews_controller.rb
@@ -2,6 +2,8 @@
 
 module AutoPreview
   class PreviewsController < ActionController::Base
+    RUBY_TYPES = %w[String Integer Float Boolean Array Hash NilClass].freeze
+
     def index
       @erb_files = find_erb_files
     end
@@ -14,12 +16,87 @@ module AutoPreview
         return
       end
 
+      @template_path = template_path
+      @locals = build_locals_from_params
+
       # Remove .html.erb suffix if present since Rails adds it back
       render_path = template_path.sub(/\.html\.erb$/, "")
-      render template: render_path, layout: false
+
+      begin
+        render template: render_path, layout: false, locals: @locals
+      rescue ActionView::Template::Error => e
+        # ActionView wraps NameError in Template::Error
+        if e.cause.is_a?(NameError)
+          handle_name_error(e.cause, template_path)
+        else
+          raise e
+        end
+      end
     end
 
     private
+
+    def build_locals_from_params
+      locals = {}
+      return locals unless params[:vars].is_a?(ActionController::Parameters) || params[:vars].is_a?(Hash)
+
+      params[:vars].each do |name, config|
+        next unless config.is_a?(ActionController::Parameters) || config.is_a?(Hash)
+
+        type = config[:type]
+        value = config[:value]
+        locals[name.to_sym] = coerce_value(value, type)
+      end
+
+      locals
+    end
+
+    def coerce_value(value, type)
+      case type
+      when "String"
+        value.to_s
+      when "Integer"
+        value.to_i
+      when "Float"
+        value.to_f
+      when "Boolean"
+        %w[true 1 yes].include?(value.to_s.downcase)
+      when "Array"
+        parse_json_or_default(value, [])
+      when "Hash"
+        parse_json_or_default(value, {})
+      when "NilClass"
+        nil
+      else
+        value.to_s
+      end
+    end
+
+    def parse_json_or_default(value, default)
+      return default if value.blank?
+
+      JSON.parse(value)
+    rescue JSON::ParserError
+      default
+    end
+
+    def handle_name_error(error, template_path)
+      # Extract undefined variable name from error message
+      # NameError messages look like: "undefined local variable or method `name' for ..."
+      match = error.message.match(/undefined local variable or method [`'](\w+)'/)
+
+      if match
+        @missing_variable = match[1]
+        @template_path = template_path
+        existing = params[:vars]
+        @existing_vars = existing.respond_to?(:keys) ? existing : {}
+        @ruby_types = RUBY_TYPES
+
+        render template: "auto_preview/previews/variable_form", layout: false
+      else
+        raise error
+      end
+    end
 
     def find_erb_files
       files = []
--- test/controllers/auto_preview/previews_controller_test.rb
+++ test/controllers/auto_preview/previews_controller_test.rb
@@ -46,6 +46,155 @@ module AutoPreview
 
       assert_response :not_found
     end
+
+    def test_show_prompts_for_missing_variable
+      get "/auto_preview/show", params: { template: "pages/greeting.html.erb" }
+
+      assert_response :success
+      assert_includes response.body, "Missing Variable"
+      assert_includes response.body, "name"
+      assert_includes response.body, "vars[name][type]"
+      assert_includes response.body, "vars[name][value]"
+    end
+
+    def test_show_renders_template_with_string_variable
+      get "/auto_preview/show", params: {
+        template: "pages/greeting.html.erb",
+        vars: { name: { type: "String", value: "Alice" } }
+      }
+
+      assert_response :success
+      assert_includes response.body, "Hello, Alice!"
+    end
+
+    def test_show_renders_template_with_integer_variable
+      get "/auto_preview/show", params: {
+        template: "pages/greeting.html.erb",
+        vars: { name: { type: "Integer", value: "42" } }
+      }
+
+      assert_response :success
+      assert_includes response.body, "Hello, 42!"
+    end
+
+    def test_show_renders_template_with_float_variable
+      get "/auto_preview/show", params: {
+        template: "pages/greeting.html.erb",
+        vars: { name: { type: "Float", value: "3.14" } }
+      }
+
+      assert_response :success
+      assert_includes response.body, "Hello, 3.14!"
+    end
+
+    def test_show_renders_template_with_boolean_true_variable
+      get "/auto_preview/show", params: {
+        template: "pages/greeting.html.erb",
+        vars: { name: { type: "Boolean", value: "true" } }
+      }
+
+      assert_response :success
+      assert_includes response.body, "Hello, true!"
+    end
+
+    def test_show_renders_template_with_boolean_false_variable
+      get "/auto_preview/show", params: {
+        template: "pages/greeting.html.erb",
+        vars: { name: { type: "Boolean", value: "false" } }
+      }
+
+      assert_response :success
+      assert_includes response.body, "Hello, false!"
+    end
+
+    def test_show_renders_template_with_nil_variable
+      get "/auto_preview/show", params: {
+        template: "pages/greeting.html.erb",
+        vars: { name: { type: "NilClass", value: "" } }
+      }
+
+      assert_response :success
+      assert_includes response.body, "Hello, !"
+    end
+
+    def test_show_renders_template_with_array_variable
+      get "/auto_preview/show", params: {
+        template: "pages/greeting.html.erb",
+        vars: { name: { type: "Array", value: '["a", "b", "c"]' } }
+      }
+
+      assert_response :success
+      # HTML-escapes quotes in the output
+      assert_includes response.body, "Hello, ["
+      assert_includes response.body, "a"
+    end
+
+    def test_show_renders_template_with_hash_variable
+      get "/auto_preview/show", params: {
+        template: "pages/greeting.html.erb",
+        vars: { name: { type: "Hash", value: '{"key": "value"}' } }
+      }
+
+      assert_response :success
+      # HTML-escapes quotes in the output
+      assert_includes response.body, "Hello, {"
+      assert_includes response.body, "key"
+    end
+
+    def test_show_preserves_existing_vars_in_form
+      # First request triggers the form
+      get "/auto_preview/show", params: {
+        template: "pages/multi_var.html.erb",
+        vars: { first_var: { type: "String", value: "hello" } }
+      }
+
+      assert_response :success
+      # Should show the form for the next missing variable
+      # and preserve the existing var in hidden fields
+      assert_includes response.body, "first_var"
+      assert_includes response.body, "second_var"
+    end
+
+    def test_show_renders_multi_var_template_with_all_vars
+      get "/auto_preview/show", params: {
+        template: "pages/multi_var.html.erb",
+        vars: {
+          first_var: { type: "String", value: "hello" },
+          second_var: { type: "String", value: "world" }
+        }
+      }
+
+      assert_response :success
+      assert_includes response.body, "First: hello"
+      assert_includes response.body, "Second: world"
+    end
+
+    def test_show_handles_empty_vars_param
+      get "/auto_preview/show", params: {
+        template: "pages/greeting.html.erb",
+        vars: {}
+      }
+
+      assert_response :success
+      assert_includes response.body, "Missing Variable"
+    end
+
+    def test_show_handles_invalid_vars_format
+      get "/auto_preview/show", params: {
+        template: "pages/greeting.html.erb",
+        vars: "invalid"
+      }
+
+      assert_response :success
+      assert_includes response.body, "Missing Variable"
+    end
+
+    def test_show_raises_non_name_error_template_errors
+      get "/auto_preview/show", params: { template: "pages/error_test.html.erb" }
+
+      # Non-NameError template errors should propagate and result in server error
+      assert_response :internal_server_error
+    end
   end
 
   class PreviewsControllerUnitTest < ActiveSupport::TestCase
@@ -59,6 +208,80 @@ module AutoPreview
       end
     ensure
       FileUtils.rm_rf(temp_dir) if temp_dir
+    end
+
+    def test_coerce_value_string
+      controller = PreviewsController.new
+      assert_equal "hello", controller.send(:coerce_value, "hello", "String")
+      assert_equal "123", controller.send(:coerce_value, 123, "String")
+    end
+
+    def test_coerce_value_integer
+      controller = PreviewsController.new
+      assert_equal 42, controller.send(:coerce_value, "42", "Integer")
+      assert_equal 0, controller.send(:coerce_value, "not a number", "Integer")
+    end
+
+    def test_coerce_value_float
+      controller = PreviewsController.new
+      assert_equal 3.14, controller.send(:coerce_value, "3.14", "Float")
+      assert_equal 0.0, controller.send(:coerce_value, "not a number", "Float")
+    end
+
+    def test_coerce_value_boolean
+      controller = PreviewsController.new
+      assert_equal true, controller.send(:coerce_value, "true", "Boolean")
+      assert_equal true, controller.send(:coerce_value, "1", "Boolean")
+      assert_equal true, controller.send(:coerce_value, "yes", "Boolean")
+      assert_equal true, controller.send(:coerce_value, "YES", "Boolean")
+      assert_equal false, controller.send(:coerce_value, "false", "Boolean")
+      assert_equal false, controller.send(:coerce_value, "anything", "Boolean")
+    end
+
+    def test_coerce_value_array
+      controller = PreviewsController.new
+      assert_equal ["a", "b"], controller.send(:coerce_value, '["a", "b"]', "Array")
+      assert_equal [], controller.send(:coerce_value, "invalid json", "Array")
+      assert_equal [], controller.send(:coerce_value, "", "Array")
+    end
+
+    def test_coerce_value_hash
+      controller = PreviewsController.new
+      assert_equal({ "key" => "value" }, controller.send(:coerce_value, '{"key": "value"}', "Hash"))
+      assert_equal({}, controller.send(:coerce_value, "invalid json", "Hash"))
+      assert_equal({}, controller.send(:coerce_value, "", "Hash"))
+    end
+
+    def test_coerce_value_nil
+      controller = PreviewsController.new
+      assert_nil controller.send(:coerce_value, "anything", "NilClass")
+    end
+
+    def test_coerce_value_unknown_type_defaults_to_string
+      controller = PreviewsController.new
+      assert_equal "hello", controller.send(:coerce_value, "hello", "UnknownType")
+    end
+
+    def test_handle_name_error_raises_for_non_matching_error
+      controller = PreviewsController.new
+      controller.params = ActionController::Parameters.new({})
+
+      # Create a NameError with an unexpected message format
+      error = NameError.new("some other error message")
+
+      assert_raises(NameError) do
+        controller.send(:handle_name_error, error, "pages/test.html.erb")
+      end
+    end
+
+    def test_build_locals_skips_invalid_config_format
+      controller = PreviewsController.new
+      controller.params = ActionController::Parameters.new({
+        vars: { name: "just a string, not a hash" }
+      })
+
+      result = controller.send(:build_locals_from_params)
+      assert_equal({}, result)
     end
   end
 end
